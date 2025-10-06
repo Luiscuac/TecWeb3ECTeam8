cc2d387e7287c086e267c6f8f52b60cfbfb5df75
Maria <mariajesuslozada10@gmail.com>
2025-10-06 14:03:33 -0400

    feat:createTicketDto

---
+++ Models/dtos/CreateTicketDto.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace apiwithdb.Models.dtos
+{
+    public class CreateTicketDto
+    {
+        [Required, StringLenght(200)]
+        public string notes { get; set; } = string.Empty;
+    }
+}

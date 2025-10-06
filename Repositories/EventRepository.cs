9358d8a89559e80cefb872b75a86dc58b58900ed
Luiscuac <luiguiandre2208@gmail.com>
2025-10-05 22:25:26 -0400

    feat:EventRepository

---
+++ Repositories/EventRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TecWeb3ECTeam8.Models;
+
+namespace TecWeb3ECTeam8.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly AppDbContext _context;
+        public EventRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Event evt)
+        {
+            await _context.Events.AddAsync(evt);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var evt = await _context.Events.FirstOrDefaultAsync(x => x.Id == id);
+            if (evt != null)
+            {
+                _context.Events.Remove(evt);
+            }
+           
+        }
+
+        public async Task<IEnumerable<Event>> GetAll()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        public async Task<Event?> GetById(Guid id)
+        {
+            return await _context.Events.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+    }
+}
---
+++ Repositories/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using TecWeb3ECTeam8.Models;
+
+namespace TecWeb3ECTeam8.Repositories
+{
+    public interface IEventRepository
+    {
+        Task <IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task Add(Event evt);
+        Task Delete(Guid id);
+    }
+}

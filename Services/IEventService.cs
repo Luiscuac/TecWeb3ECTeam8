5fe48e5a706fc1c516bb664a50df78f0b3c717a1
Luiscuac <luiguiandre2208@gmail.com>
2025-10-06 13:59:03 -0400

    feat:EventService

---
+++ Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using TecWeb3ECTeam8.Models;
+using TecWeb3ECTeam8.Models.dtos;
+
+namespace TecWeb3ECTeam8.Services
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task<Event> Create(CreateEventDto dto);
+        Task<bool> Delete(Guid id);
+    }
+}

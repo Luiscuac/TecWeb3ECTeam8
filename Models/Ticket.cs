8d67aabe813ec0cd96e128e122c06b353b58fd0a
Maria <mariajesuslozada10@gmail.com>
2025-10-06 13:34:48 -0400

    feat: ticket model

---
+++ Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiwithdb.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+
+        public string notes { get; set; } = string.Empty;
+    }
+}

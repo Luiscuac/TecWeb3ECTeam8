a8fbe6007748e37307985ce86d58fc5b871f2a6a
Luiscuac <luiguiandre2208@gmail.com>
2025-10-05 22:50:47 -0400

    feat:Data Dbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TecWeb3ECTeam8.Models;
+
+namespace TecWeb3ECTeam8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Books => Set<Event>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>(e =>
+            {
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Title).IsRequired().HasMaxLength(200);
+                e.Property(x => x.Date).IsRequired();
+                e.Property(x => x.Capacity).IsRequired();
+            });
+        }
+    }
+}

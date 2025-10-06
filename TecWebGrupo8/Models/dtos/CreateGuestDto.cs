9f0ad28387a09dfd77143dd20bbe84b78223b6ea
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-05 22:18:51 -0400

    feat(models): adding guest dto

---
+++ TecWebGrupo8/Models/dtos/CreateGuestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecWebGrupo8.Models.dtos;
+
+public class CreateGuestDto
+{
+    [Required, StringLength(200)]
+    public string FullName { get; set; } = string.Empty;
+
+    public bool Confirmed { get; set; } = false;
+}

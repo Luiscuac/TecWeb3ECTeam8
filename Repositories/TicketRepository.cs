9ee5cacc2aa5b3d361421930eb52da26a47c11d8
Maria <mariajesuslozada10@gmail.com>
2025-10-06 14:11:31 -0400

    feat: adding ticketRepository

---
+++ Repositories/TicketRepository.cs
@@ -0,0 +1,43 @@
+﻿using apiwithdb.Data;
+using apiwithdb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace apiwithdb.Repositories
+{
+    public class TicketRepository: ITicketRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public TicketRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Ticket ticket)
+        {
+            await _context.Tickets.AddAsync(ticket);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var ticket = await _context.Tickets.FirstOrDefaultAsync(x => x.Id == id);
+            if (ticket != null)
+            {
+                _context.Tickets.Remove(ticket);
+            }
+        }
+
+        public async Task<IEnumerable<Ticket>> GetAll()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+
+        public async Task<Ticket?> GetById(Guid id)
+        {
+            return await _context.Tickets.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        
+    }
+
+}
+

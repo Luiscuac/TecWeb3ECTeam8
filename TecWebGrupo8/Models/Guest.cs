f899e7c1a1aaf8bdd4e4eb95e3976ea730b4ff66
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-05 22:17:05 -0400

    feat(models): adding guest.cs

---
+++ TecWebGrupo8/Models/Guest.cs
@@ -0,0 +1,8 @@
+﻿namespace TecWebGrupo8.Models;
+
+public class Guest
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public bool Confirmed { get; set; }
+}

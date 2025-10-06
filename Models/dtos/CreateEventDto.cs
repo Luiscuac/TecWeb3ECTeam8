292dbbb436e8d16b03cd31aebb12e6ee32a7a902
Luiscuac <luiguiandre2208@gmail.com>
2025-10-06 13:52:21 -0400

    feat:dtoCreateEvent

---
+++ Models/dtos/CreateEventDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecWeb3ECTeam8.Models.dtos
+{
+    public class CreateEventDto
+    {
+        [Required, StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        [Range(1, 1000)]
+        public int Capacity { get; set; }
+    }
+}

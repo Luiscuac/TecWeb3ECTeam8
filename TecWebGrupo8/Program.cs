0919e5d53ad3135053d21acbd4f4dad49f9f1246
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-05 22:29:16 -0400

    feat: adding injections in Program.cs

--- TecWebGrupo8/Program.cs
+++ TecWebGrupo8/Program.cs
@@ -1,18 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TecWebGrupo8.Data;
+using TecWebGrupo8.Repositories;
+using TecWebGrupo8.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ---------- Services ----------
 
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// OpenAPI (plantilla .NET 8)
 builder.Services.AddOpenApi();
+
+// DbContext (ajusta el provider si no usas SQL Server)
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// DI para Guest
+builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+builder.Services.AddScoped<IGuestService, GuestService>();
+
+// (Opcional) CORS si vas a consumir desde un front local
+// builder.Services.AddCors(o => o.AddDefaultPolicy(p =>
+//     p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ---------- Pipeline ----------
+
 if (app.Environment.IsDevelopment())
 {
+    // UI de OpenAPI (según plantilla de .NET 8)
     app.MapOpenApi();
 }
+
+// app.UseCors(); // si activaste CORS arriba
 
 app.UseHttpsRedirection();
 

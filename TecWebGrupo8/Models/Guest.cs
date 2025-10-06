99bb25c064ee4f9482c59190750ac452ebdc9c82
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-06 12:07:38 -0400

    refactor: mover contenido de TecWebGrupo8/* a carpetas en la raíz

--- TecWebGrupo8/Models/Guest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TecWebGrupo8.Models;
-
-public class Guest
-{
-    public Guid Id { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public bool Confirmed { get; set; }
-}
